d5f870bff8c6d5792a995d952158fd8dc93e3df7
Amir Mansoor <69566729+amir-mansoor@users.noreply.github.com>
2023-03-28 11:56:29 +0300

    basic expressions

---
+++ python_class_1.py
@@ -0,0 +1,73 @@
+# Expressions
+# Lets start with some basic expression.
+
+17
+
+# Let see it in detail
+## + operator
+## -2 and 15 are operands
+# binary operator
+
+# -2 + 15
+
+### Now let see devision
+
+#print(4 / 2)
+#print(1 / 2)
+
+### REMAINDER
+#print(15 % 3)
+#print(15 % 2)
+
+# TYPE
+# type(1)
+# type(1.0)
+
+# 2 + 2 * 4
+#(2 + 2) * 2
+
+
+# CONDITIONS
+
+# 3 < 4
+# 3 == 4
+# 3 = 3 # IGNORE THE DETAILS
+# 3 >= 4
+
+
+# OTHER TYPES OF Expressions
+# 'ABC'
+# "xyz"
+# """
+# This is called a multi-line string.
+# Span multiple lines.
+# """
+# type("ABC")
+# "ab"+"cd"
+# 'a' * 5
+
+# True
+# False
+
+# True and True
+# True and False
+# False and True
+# False and False
+
+# OR
+# True or True
+# True or False
+# False or False
+# And prcedence is more than or the first execute and
+# True or False and False or False
+# (True or False) and (False or False)
+
+# Call Expressions
+# max(7,9)
+# pow(100,2)
+
+# Nesting
+#print(max(min(1,-2), min(pow(3,5),-1)))
+
+# Undefined Variables
+# v
