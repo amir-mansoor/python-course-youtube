06cbe78f169941dfe4d8b2302c0f461f94767a9b
Amir Mansoor <69566729+amir-mansoor@users.noreply.github.com>
2023-04-25 12:28:53 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+# How to create functions in python
+
+# Example 1
+
+def add_numbers(a,b):
+    return a + b
+
+# def is short for define
+# a and b are parameters
+
+# Example 2
+def sub_numbers(a,b):
+    return a - b
+
+# How to call a function
+
+print(add_numbers(5,7))
+
+# See the answer. the answer is 12 5+7 = 12
+
+# Call to subtract function
+print(sub_numbers(5,7))
+# The answer is -2 
+
+# Hope you guys like the video.
+# Also subscribe the channel.
